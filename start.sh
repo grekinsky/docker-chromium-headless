f85e52fb19229fd2d3ea5b719df6c4831eed6865
greco <greco@paradigma.com.mx>
2017-05-01 13:43:08 -0500

    Improve return value from script

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-export DISPLAY=:1
+export DISPLAY=:1;
 Xvfb $DISPLAY -ac -screen 0 1280x1024x8 & \
     yarn run test
+RET1=$?;
+RET2=0;
 if [ "$RESULTFILE" != "" ]; then
-    chmod 666 $RESULTFILE;
-    echo "Changed permissions to result file $RESULTFILE";
-    exit 0;
+    chmod 666 $RESULTFILE && \
+        echo "Changed permissions to result file $RESULTFILE"
+    RET2=$?:
 fi
+exit $RET1 && $RET2;
